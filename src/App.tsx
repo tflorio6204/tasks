fe8d471118e8740143b7e9b1fb78ebc2bab7d170
Tommy <tflorio@udel.edu>
2024-08-29 21:00:33 -0400

    Added name to app!

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
+                Thomas Florio:
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
