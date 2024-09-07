8a09a8fc32cde781e6e41c0ca95e03149aee361d
Thomas <tflorio@udel.edu>
2024-09-07 12:19:13 -0400

    Completed shout function

--- src/functions.ts
+++ src/functions.ts
@@ -31,7 +31,8 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    message = message.toUpperCase() + "!";
+    return message;
 }
 
 /**
