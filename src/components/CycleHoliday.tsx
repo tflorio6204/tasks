d8d7b1d563d5410dcc8a0fa3a86bebcd229953aa
Thomas <tflorio@udel.edu>
2024-09-19 14:10:08 -0400

    Saving Holidays

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,21 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): React.JSX.Element {
+    type holidays = "🎄" | "🦃" | "🐇" | "🎃" | "🎆";
+    const [holiday, setHoliday] = useState<string>("🎄");
+    const ALPHABETICAL_TRANSITIONS: Record<holidays, holidays> = {
+        "🎄": "🐇",
+        "🐇": "🎃",
+        "🎃": "🎆",
+        "🎆": "🦃",
+    };
+    const TOY_TRANSITIONS: Record<holidays, holidays> = {
+        "🎃": "🦃",
+        "🦃": "🎄",
+        "🎄": "🎆",
+        "🎆": "🐇",
+    };
     return <div>Cycle Holiday</div>;
 }
+// C, E, H, N, T
+// Decemeber, November, March/April, October, Janurary
