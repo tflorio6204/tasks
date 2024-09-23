185f5d09dbf19b9a2392d49ab4260df9b6299600
Thomas <tflorio@udel.edu>
2024-09-23 15:28:55 -0400

    Working on checkAnswer

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
-export function CheckAnswer({expectedAnswer}: {expectedAnswer: string}): React.JSX.Element 
-{
-    const userInput[answer, setAnswer] = useState<string>("");
-     return (
+export function CheckAnswer({
+    expectedAnswer,
+}: {
+    expectedAnswer: string;
+}): React.JSX.Element {
+    //const userInput[answer, setAnswer] = useState<string>("");
+    return (
         <div>
             <h3>Check Answer</h3>
-            
+            <Form.Label>Name:</Form.Label>
         </div>
     );
 }
