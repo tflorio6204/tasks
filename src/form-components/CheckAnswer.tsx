9c700aefeef0497731d6cf50f986a21f819ea6e0
Thomas <tflorio@udel.edu>
2024-09-23 10:22:30 -0400

    Taking a break

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 
-export function CheckAnswer({
-    expectedAnswer,
-}: {
-    expectedAnswer: string;
-}): React.JSX.Element {
-    return (
+export function CheckAnswer({expectedAnswer}: {expectedAnswer: string}): React.JSX.Element 
+{
+    const userInput[answer, setAnswer] = useState<string>("");
+     return (
         <div>
             <h3>Check Answer</h3>
+            
         </div>
     );
 }
