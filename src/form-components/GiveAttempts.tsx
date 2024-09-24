14f0e66d0909db05b87f77973b8681ef890b62dc
Thomas <tflorio@udel.edu>
2024-09-24 16:10:14 -0400

    Completed GiveAttempts

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,45 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): React.JSX.Element {
+    const [userAttempts, setAttemptNum] = useState<number>(3);
+    const [newRequest, setNewRequest] = useState<number>(0);
+
+    const requestAttempt = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (!Number.isNaN(parseInt(event.target.value))) {
+            setNewRequest(parseInt(event.target.value));
+        }
+    };
     return (
         <div>
-            <h3>Give Attempts</h3>
+            <Form.Group controlId="totalAttempt">
+                <Form.Label>
+                    Attempts Left: {userAttempts}
+                    <Form.Control
+                        type="number"
+                        value={newRequest}
+                        onChange={requestAttempt}
+                    ></Form.Control>
+                </Form.Label>
+            </Form.Group>
+            <div>
+                <h3>Give Attempts</h3>
+                <Button
+                    onClick={() => {
+                        setAttemptNum(userAttempts - 1);
+                    }}
+                    disabled={userAttempts <= 0}
+                >
+                    use
+                </Button>
+                <Button
+                    onClick={() => {
+                        setAttemptNum(userAttempts + newRequest);
+                    }}
+                >
+                    gain
+                </Button>
+            </div>
         </div>
     );
 }
