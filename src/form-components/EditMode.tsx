c62547792d69b6991b12cc6382ad3ae04bfa12a5
Thomas <tflorio@udel.edu>
2024-09-24 17:51:05 -0400

    Completed EditMode

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -16,27 +16,29 @@ export function EditMode(): React.JSX.Element {
     };
     return (
         <div>
-            Switch Mode?
-            <Form.Check
-                type="switch"
-                id="isEditable"
-                checked={editable}
-                onChange={changeMode}
-            />
-            Are you a student?
-            <Form.Check
-                type="checkbox"
-                id="isStudent"
-                checked={student}
-                onChange={changeVerification}
-                disabled={!editable}
-            />
+            <Form.Group>
+                <Form.Check
+                    type="switch"
+                    id="isEditable"
+                    label="Edit Mode?"
+                    checked={editable}
+                    onChange={changeMode}
+                />
+            </Form.Group>
             <h3>Edit Mode</h3>
             {!editable ?
                 student ?
                     <div> {username} is a student </div>
                 :   <div> {username} is not a student </div>
             :   <div>
+                    <Form.Check
+                        type="checkbox"
+                        id="student"
+                        label="Are you a student?"
+                        checked={student}
+                        onChange={changeVerification}
+                        disabled={!editable}
+                    />
                     <Form.Label>
                         Name:
                         <Form.Control
