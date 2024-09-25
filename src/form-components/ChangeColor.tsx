f74d0dd8a4bccc2095c71486cfc9c73b9868f2af
Thomas <tflorio@udel.edu>
2024-09-25 16:08:49 -0400

    Taking a break

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,34 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function ChangeColor(): React.JSX.Element {
+    const colorList = [
+        "Yellow",
+        "Red",
+        "Blue",
+        "Green",
+        "Pink",
+        "Black",
+        "Orange",
+        "White",
+    ];
+    const [color, setColor] = useState<string>(colorList[0]);
+    const updateColor = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setColor(event.target.value);
+    };
     return (
         <div>
             <h3>Change Color</h3>
+            {colorList.map((chosenColor: string) => (
+                <Form.Check
+                    inline
+                    type="radio"
+                    name="color"
+                    onChange={updateColor}
+                    label={chosenColor}
+                    key={chosenColor}
+                />
+            ))}
         </div>
     );
 }
